3239bdcc0ef541613ad7b8d23defc961a6e51a82
Guany <1084072690@qq.com>
2022-04-10 22:34:57 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:tlyboy/blog.git main:gh-pages
-git push -f git@gitee.com:tlyboy/blog.git main:gh-pages
+git push -f git@github.com:tlyboy/blog.git master:gh-pages
+git push -f git@gitee.com:tlyboy/blog.git master:gh-pages
 
 cd -
 
