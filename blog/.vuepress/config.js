658534839469d022feda92c4459b93960dd5a7e6
Guany <1084072690@qq.com>
2022-05-15 18:23:08 +0800

    update

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
       'meta',
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, user-scalable=no'
+        content: 'width=device-width, initial-scale=1.0, user-scalable=no'
       }
     ],
     [
